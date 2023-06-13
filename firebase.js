3c313c71e97588fc2be254ad5292fe3a91482934
Walter <w.angoye@alustudent.com>
2023-06-13 10:34:12 +0300

    Updated firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCjclGsz29IOhmfBD4jNn2nnElLC5j5ht0",
@@ -10,3 +11,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const notesCollection = collection(db, "notes");
