fdd76ecd706dc96cda9cee5fead7bd1d139d19f7
Walter <w.angoye@alustudent.com>
2023-06-13 10:26:20 +0300

    Updated firebase.js

--- firebase.js
+++ firebase.js
@@ -13,3 +13,21 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
 export const notesCollection = collection(db, "notes")
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCjclGsz29IOhmfBD4jNn2nnElLC5j5ht0",
+//   authDomain: "react-notes-f2366.firebaseapp.com",
+//   projectId: "react-notes-f2366",
+//   storageBucket: "react-notes-f2366.appspot.com",
+//   messagingSenderId: "999656511692",
+//   appId: "1:999656511692:web:6880fc67c817cb26004a69"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
